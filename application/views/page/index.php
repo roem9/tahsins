b62d87647c54b7bb4cb70f6fe38b6403cdefef18
roem9 <muhammadrum04@gmail.com>
2020-06-23 14:58:25 +0700

    2

--- application/views/page/index.php
+++ application/views/page/index.php
@@ -1,11 +1,26 @@
 <div class="container">
             <div class="row">
-                <!-- Mufrodat 1 -->
                 <div class="col-12 col-md-4 mb-3">
                     <div class="card border-success">
                         <div class="card-header list-group-item-success d-flex justify-content-between">Hal. 1</div>
                         <div class="card-body">
                             <a href="<?= base_url()?>?page=<?= MD5('hal 1')?>" class="btn btn-block btn-sm btn-success"><i class="fa fa-book mr-1"></i> Belajar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <div class="card border-success">
+                        <div class="card-header list-group-item-success d-flex justify-content-between">Hal. 2</div>
+                        <div class="card-body">
+                            <a href="<?= base_url()?>?page=<?= MD5('hal 2')?>" class="btn btn-block btn-sm btn-success"><i class="fa fa-book mr-1"></i> Belajar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 mb-3">
+                    <div class="card border-success">
+                        <div class="card-header list-group-item-success d-flex justify-content-between">Hal. 3</div>
+                        <div class="card-body">
+                            <a href="<?= base_url()?>?page=<?= MD5('hal 3')?>" class="btn btn-block btn-sm btn-success"><i class="fa fa-book mr-1"></i> Belajar</a>
                         </div>
                     </div>
                 </div>
--- application/views/page/page.php
+++ application/views/page/page.php
@@ -1,22 +1,37 @@
 <div class="container">
             <div class="row">
                 <!-- Mufrodat 1 -->
-                <div class="col-12 col-md-4 mb-3">
-                    <ul class="list-group">
-                        <?php foreach ($kata as $kata) :?>
-                            <li class="list-group-item text-right arab">
-                                <?= $kata['kata'];?>
-                            </li>
-                            <li class="list-group-item text-center">
-                                <audio controls>
-                                    <source src="<?= base_url()?>sound/1.1.ogg" type="audio/ogg">
-                                    Your browser does not support the audio element.
-                                </audio>
-                                <!-- <span class="btn btn-sm btn-info"><i class="fa fa-play mr-1"></i>Putar</span> -->
-                            </li>
-                        <?php endforeach;?>
-                    </ul>
-                </div>
+                <?php foreach ($kata as $kata) :?>
+                    <div class="col-12 col-md-4 mb-3">
+                        <ul class="list-group">
+                                <li class="list-group-item text-right arab">
+                                    <?= $kata['kata'];?>
+                                </li>
+                                <li class="list-group-item text-center">
+                                    <audio controls>
+                                        <!-- <source src="<?= base_url()?>sound/1.1.ogg" type="audio/ogg"> -->
+                                        <source src="<?= base_url()?>sound/<?= $kata['audio']?>" type="audio/mpeg">
+                                        <source src="<?= base_url()?>sound/<?= $kata['audio']?>" type="audio/mp4" />
+                                        Your browser does not support the audio element.
+                                    </audio>
+                                </li>
+                        </ul>
+                    </div>
+                <?php endforeach;?>
+                <?php if(!empty($next) && !empty($back)):?>
+                    <div class="col-12 d-flex justify-content-between">
+                        <a href="<?= $back?>" class="btn btn-success btn-sm"><i class="fa fa-arrow-left"></i></a>
+                        <a href="<?= $next?>" class="btn btn-success btn-sm"><i class="fa fa-arrow-right"></i></a>
+                    </div>
+                <?php elseif(!empty($next) && empty($back)):?>
+                    <div class="col-12 d-flex justify-content-end">
+                        <a href="<?= $next?>" class="btn btn-success btn-sm"><i class="fa fa-arrow-right"></i></a>
+                    </div>
+                <?php elseif(empty($next) && !empty($back)):?>
+                    <div class="col-12 d-flex justify-content-start">
+                        <a href="<?= $next?>" class="btn btn-success btn-sm"><i class="fa fa-arrow-right"></i></a>
+                    </div>
+                <?php endif;?>
             </div>
         </div>
     </div>
